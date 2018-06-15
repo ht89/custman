c6f13ea1ecd2595cb1f6f237908478f8077814b0
Hung Tran <hung.tran@swissitbridge.com>
2018-06-15 15:39:11 +0700

    finished sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 // import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { ROUTES } from './app.routes';
 import { CustomersModule } from './customers/customers.module';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { ProductsModule } from './products/products.module';
 import { SharedComponent } from './shared/shared.component';
 import { OrdersModule } from './orders/orders.module';
-
+import {SidebarModule} from 'primeng/sidebar';
 
 @NgModule({
     declarations: [
@@ -28,13 +28,17 @@ import { OrdersModule } from './orders/orders.module';
         BrowserModule,
         BrowserAnimationsModule,
         RouterModule.forRoot(ROUTES),
+
         AngularFireModule.initializeApp(environment.firebase),
         AngularFirestoreModule, // for database features
         AngularFireAuthModule,
         // AngularFireStorageModule // for storage features
+
         CustomersModule,
         ProductsModule,
-        OrdersModule
+        OrdersModule,
+
+        SidebarModule
     ],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+    styleUrls: ['./header.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
+    showSideBar = false;
 
     constructor(public router: Router) { }
 
