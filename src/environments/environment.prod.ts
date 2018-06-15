ed86f49c3a1024465f187052c206fa8a7e71c38c
Hung Tran <hung.tran@swissitbridge.com>
2018-06-15 17:49:05 +0700

    corrected the hosting path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyDg7Ef69HnHw-4lRCp74OKVHKdmm6z_xT8',
+        authDomain: 'custman-1466e.firebaseapp.com',
+        databaseURL: 'https://custman-1466e.firebaseio.com',
+        projectId: 'custman-1466e',
+        storageBucket: '',
+        messagingSenderId: '874435792659'
+    }
 };
