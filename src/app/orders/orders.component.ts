3c1093bb3bd527d85f89363dec3db0bf23e0b563
Hung Tran <hung.tran@swissitbridge.com>
2018-06-19 13:56:55 +0700

    finished customer autocomplete

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../customers/customer.interface';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/internal/Observable';
+import { CustomerId } from '../customers/customer-id.interface';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent implements OnInit {
+  private customerCollection: AngularFirestoreCollection<Customer>;
+  customers: CustomerId[];
+  filteredCustomers: CustomerId[];
 
-  constructor() { }
+  customer: CustomerId;
+
+  constructor(private readonly afs: AngularFirestore) { }
 
   ngOnInit() {
+    this.customerCollection = this.afs.collection<Customer>('customers');
+
+    this.customerCollection.snapshotChanges()
+      .pipe(
+        map(actions => actions.map(a => {
+          const data = a.payload.doc.data() as Customer;
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        }))
+      )
+      .subscribe(customers => {
+        this.customers = customers;
+      });
+  }
+
+  searchCustomer(event) {
+    console.log(event.query);
+
+    this.filteredCustomers = [];
+    for (let i = 0; i < this.customers.length; i++) {
+      if (this.customers[i].name.toLowerCase().indexOf(event.query.toLowerCase()) === 0) {
+        this.filteredCustomers.push(this.customers[i]);
+      }
+    }
+
   }
 
 }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { OrdersComponent } from './orders.component';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    CardModule
+    FormsModule,
+    CardModule,
+    AutoCompleteModule
   ],
   exports: [],
   declarations: [OrdersComponent],
